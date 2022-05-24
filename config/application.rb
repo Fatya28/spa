449c5a41043e8b07f20d8b288dc8f65f2737460a
Fatya <tima.voroshiliva@gmail.com>
2022-05-24 21:09:11 +0300

    Add config.assets.initialize_on_precompile = false to config/application.rb

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,7 @@ module Spa
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
+    config.assets.initialize_on_precompile = false
 
     # Configuration for the application, engines, and railties goes here.
     #
