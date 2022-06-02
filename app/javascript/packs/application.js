123e63ac1dffd1c5e10c3b20efaedae4988dc761
Fatya <tima.voroshiliva@gmail.com>
2022-06-02 09:04:14 +0300

    Delete folder javascript/packs

--- app/javascript/packs/application.js
+++
@@ -1,9 +0,0 @@
-// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-//= require bootstrap-datepicker
-
-import "@hotwired/turbo-rails"
-import "controllers"
-import "@fortawesome/fontawesome-free"
-import "bootstrap/dist/js/bootstrap.bundle"
-import "trix"
-import "@rails/actiontext"
