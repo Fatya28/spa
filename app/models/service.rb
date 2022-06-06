b3529cebcb94d0c610c0f5326c7682d88b246206
Fatya <tima.voroshiliva@gmail.com>
2022-06-06 12:17:46 +0300

    Update service model

--- app/models/service.rb
+++ app/models/service.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Service < ApplicationRecord
-  validates :title, :performed, :description, :introduction, :image, presence: true
+  validates :title, :performed, :description, :introduction, presence: true
 
   has_one_attached :image, dependent: :destroy
 
