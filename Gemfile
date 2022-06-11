1b388ae149b12816385ea756315c00c7f511b4d9
Fatya <tima.voroshiliva@gmail.com>
2022-06-12 01:33:04 +0300

    Add Cloudinary

--- Gemfile
+++ Gemfile
@@ -81,3 +81,4 @@ group :test do
 end
 
 gem 'bootstrap-datepicker-rails'
+gem 'cloudinary'
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,7 +5,7 @@ module ApplicationHelper
   def get_image(user_id, size = 40)
     user = User.find(user_id)
     if user.avatar.attached?
-      image_tag url_for(user.avatar), class: 'rounded-circle mx-auto', size: "#{size}x#{size}"
+      cl_image_tag(user.avatar.key, class: 'rounded-circle mx-auto', size: "#{size}x#{size}") 
     else
       image_tag 'user/default.png', class: 'rounded-circle mx-auto img-fluid', size: "#{size}x#{size}"
     end
